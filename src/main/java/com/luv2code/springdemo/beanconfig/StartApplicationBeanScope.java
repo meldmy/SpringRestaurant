3d50f443192189b9f340dca6c7a27c65c9a02a18
Dmytro Melnychuk <melnichukdv@gmail.com>
2016-11-09 19:22:01 +0100

    Added BeanContainer as @Configuration with define some beans

---
+++ src/main/java/com/luv2code/springdemo/beanconfig/StartApplicationBeanScope.java
@@ -0,0 +1,28 @@
+package com.luv2code.springdemo.beanconfig;
+
+import com.luv2code.springdemo.coaches.AlphaCricketCoach;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static java.lang.System.out;
+
+
+/**
+ * @author Dmytro Melnychuk
+ */
+public class StartApplicationBeanScope
+{
+    public static void main( String[] args )
+    {
+        // These two version of load configuration into project: 1) xml, 2) config class
+        // ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfigBeanContainer.class);
+
+        AlphaCricketCoach myCoach = context.getBean( "alphaCricketCoach", AlphaCricketCoach.class );
+        out.println(myCoach.getFortuneService().getFortune());
+        out.println(myCoach.getFortuneService().getFortune());
+        out.println(myCoach.getFortuneService().getFortune());
+        out.println(myCoach.getDailyWorkout());
+
+        context.close();
+    }
+}
